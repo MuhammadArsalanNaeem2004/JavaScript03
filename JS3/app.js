27b0675bf3bc3bae961879dd83959acdf0fb8a96
MuhammadArsalanNaeem2004 <130148088+MuhammadArsalanNaeem2004@users.noreply.github.com>
2023-04-27 15:05:04 +0500

    Assignment 03

---
+++ JS3/app.js
@@ -0,0 +1,140 @@
+// Chapter 1
+
+// alert("Hello Sir \n Welcome to Our Website");
+     
+
+// Chapter 2
+
+// var myName = "Muhammad Arsalan";
+// var Age = "18 Years Old";
+// var Certificate = "Certified In Web Development";
+
+// alert(myName+"\n\n"+Age+"\n\n"+Certificate)
+
+ // Chapter 3
+
+// var Visits= 15;
+
+// alert("You Have Visited This Site "+Visits+" Times" );
+
+// Chapter 4
+
+// Legal variable names:
+
+// age
+// fullName
+// num_students
+// $price
+// _count
+// Illegal variable names:
+
+// 1st_place (variable names cannot begin with a number)
+// full-name (variable names cannot contain hyphens)
+// break (break is a reserved keyword in JavaScript and cannot be used as a variable name)
+// my favorite color (variable names cannot contain spaces)
+// class (class is also a reserved keyword in JavaScript and cannot be used as a variable name)
+
+// Chapter 5
+
+// var birthYear = +prompt("Year Of Birth");
+// var currentYear = +prompt("Current Year");
+// var age = currentYear - birthYear;
+// alert("Your Age is "+ age);
+
+// Chapter 6 - 9
+
+
+// var subject1 = prompt("Enter Subject 1:");
+// var subject2 = prompt("Enter Subject 2:");
+// var subject3 = prompt("Enter Subject 3:");
+
+
+// var totalMarks = 100;
+
+
+// var obtainedMarksSubject1 = parseFloat(prompt("Enter obtained marks for " + subject1 + ":"));
+
+
+// var obtainedMarksSubject2 = parseFloat(prompt("Enter obtained marks for " + subject2 + ":"));
+// var obtainedMarksSubject3 = parseFloat(prompt("Enter obtained marks for " + subject3 + ":"));
+
+
+// var totalObtainedMarks = obtainedMarksSubject1 + obtainedMarksSubject2 + obtainedMarksSubject3;
+// var percentage = (totalObtainedMarks / (totalMarks  3))  100;
+
+// document.write("<h1>Marks Sheet</h1>");
+// document.write("<br>");
+// document.write("Subject 1: " + subject1 + "<br>");
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Obtained Marks: " + obtainedMarksSubject1 + "<br>");
+// document.write("Percentage: " + (obtainedMarksSubject1 / totalMarks) * 100 + "%<br>");
+// document.write("<br>");
+// document.write("Subject 2: " + subject2 + "<br>");
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Obtained Marks: " + obtainedMarksSubject2 + "<br>");
+// document.write("Percentage: " + (obtainedMarksSubject2 / totalMarks) * 100 + "%<br>");
+// document.write("<br>");
+// document.write("Subject 3: " + subject3 + "<br>");
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Obtained Marks: " + obtainedMarksSubject3 + "<br>");
+// document.write("Percentage: " + (obtainedMarksSubject3 / totalMarks) * 100 + "%<br>");
+// document.write("<br>");
+// document.write("Total Marks: " + (totalMarks * 3) + "<br>");
+// document.write("Total Obtained Marks: " + totalObtainedMarks + "<br>");
+// document.write("Overall Percentage: " + percentage.toFixed(2) + "%<br>");
+
+// Chapter 9-11
+
+// var firstNumber = +prompt("Enter First number"); 
+// var secondNumber = +prompt("Enter Second number");
+
+// var operation = prompt("Enter Opteration : +, -, *, /, %")
+
+// if (operation == "+") {
+//     alert(firstNumber + secondNumber);
+    
+// }
+// else if (operation == "-") {
+//     alert(firstNumber - secondNumber);
+// }
+// else if (operation == "*") {
+//     alert(firstNumber * secondNumber);
+// }
+// else if (operation == "/") {
+//     alert(firstNumber / secondNumber);
+// }
+// else if (operation == "%") {
+//     alert(firstNumber % secondNumber);
+// }
+// else {
+//     alert("Number Not Found");
+// }
+
+// Chapter 12-13
+
+// var password = prompt("Enter Password");
+
+// if (password == "123") {
+//     alert("Greeting Sir");
+// } else {
+//     alert("Invalid Password");
+// }
+// // Chapter 14-16
+
+// var x = ["SSC","HSC","BCS","BS","BCOM","MS"];
+
+// for (var i=0; i < x.length ; ++i){
+//     document.write(x[i]+"<br>");
+//     ;
+ 
+// }
+
+// Chapter 14-16
+
+// var x = ["SSC","HSC","BCS","BS","BCOM","MS"];
+
+// for (var i=0; i < x.length ; ++i){
+//     document.write(x[i]+"<br>");
+//     ;
+ 
+// }
